5e336f451aee86afe51b20a594f3c6425e6cfac8
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-03-21 14:11:12 +0100

    suits tests

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,11 +1,53 @@
 import {test} from '@playwright/test';
 
-test('the first test', async ({page}) => {
-       await page.goto('http://localhost:4200/')
-       await page.getByText('Forms').click()
+
+test.beforeEach(async ({page}) => {
+    await page.goto('http://localhost:4200/')
+    
+})
+
+
+
+test.describe('suite1', () => {
+
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Auth').click()
+    })
+
+    test('the login test', async ({page}) => {
+       
+       await page.getByText('Login').click()
+
+    })
+
+    test('the register test', async ({page}) => {
+       
+       await page.getByText('Register').click()
+
+    })
+
+
+
+})
+
+test.describe('suite2', () => {
+
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Forms').click()
+    })
+
+    test('the first test', async ({page}) => {
+       
        await page.getByText('Form Layouts').click()
 
-});
+    })
 
+    test('navigate to datepicker page', async ({page}) => {
+       
+       await page.getByText('Datepicker').click()
+
+    })
+
+})
 
 
