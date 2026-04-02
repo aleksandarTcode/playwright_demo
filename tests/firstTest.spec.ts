955f0862eec24bf03e4480a157bcffae420a006b
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-04-02 09:39:10 +0200

    locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,52 +2,54 @@ import {test} from '@playwright/test';
 
 
 test.beforeEach(async ({page}) => {
-    await page.goto('http://localhost:4200/')
-    
+    await page.goto('http://localhost:4200/');
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layouts').click()
+
+})
+
+
+test('Locator syntax rules', async ({page}) => {
+
+    //by Tag name
+    await page.locator('input').first().click()
+
+    //by ID
+    page.locator('#inputEmail1')
+
+    //by Class value
+    page.locator('.shape-rectangle')
+
+    //by Attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"].shape-rectangle')
+
+    //by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+
+    //by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
+
+
+
+
+
+
+
+       
+
 })
 
 
 
-test.describe('suite1', () => {
 
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Auth').click()
-    })
-
-    test('the login test', async ({page}) => {
-       
-       await page.getByText('Login').click()
-
-    })
-
-    test('the register test', async ({page}) => {
-       
-       await page.getByText('Register').click()
-
-    })
-
-
-
-})
-
-test.describe('suite2', () => {
-
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Forms').click()
-    })
-
-    test('the first test', async ({page}) => {
-       
-       await page.getByText('Form Layouts').click()
-
-    })
-
-    test('navigate to datepicker page', async ({page}) => {
-       
-       await page.getByText('Datepicker').click()
-
-    })
-
-})
 
 
