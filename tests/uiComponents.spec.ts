6c99dacaadbf8af88c09a9eeda7a2ce8449b6325
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-04-03 11:39:08 +0200

    dialog box

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -120,3 +120,22 @@ test('tooltips', async({page}) => {
 
 })
 
+
+test('dialog box', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+    
+    await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
+
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+
+
+
+
+})
+
