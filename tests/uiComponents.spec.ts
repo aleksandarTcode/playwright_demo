6f756ee8da64d605bb8518c526782bacbd4f5a1a
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-04-03 09:35:36 +0200

    radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -28,7 +28,26 @@ test.describe('Form Layouts page', () => {
         await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
     })
 
-  
+    test('radio buttons', async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        // await usingTheGridForm.getByLabel('Option 1').check({force: true}) //force is needed because in this case radio button has class visually-hidden
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true})
+
+        //generic assertion
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+
+        //locator assertion
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
+
+
+    })
 
 
 })
