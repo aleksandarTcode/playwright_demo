71c6bf4a9de8a67bb8fadf1e722eac283c0ec9f5
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-04-06 09:47:52 +0200

    navigation page object

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -9,8 +9,44 @@ export class NavigationPage {
     }
 
     async formLayoutsPage(){
-        await this.page.getByText('Forms').click()
+        await this.selectGroupMenuItem('Forms') //checks if Forms is expanded or collapsed
         await this.page.getByText('Form Layouts').click()
+    }
+
+    async datepickerPage(){
+        // await this.page.getByText('Forms').click()
+        await this.selectGroupMenuItem('Forms')
+        // await this.page.waitForTimeout(1000)
+        await this.page.getByText('Datepicker').click()
+
+    }
+
+    async smartTablePage(){
+        await this.selectGroupMenuItem('Tables & Data')
+        await this.page.getByText('Smart Table').click()
+
+    }
+
+    async toastrPage(){
+        await this.selectGroupMenuItem('Modal & Overlays')
+        await this.page.getByText('Toastr').click()
+
+    }
+
+    async tooltipPage(){
+        await this.selectGroupMenuItem('Modal & Overlays')
+        await this.page.getByText('Tooltip').click()
+
+    }
+
+    private async selectGroupMenuItem(groupItemTitle: string){
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+
+        if(expandedState == "false"){
+            await groupMenuItem.click()
+        }
+
     }
 
 }
