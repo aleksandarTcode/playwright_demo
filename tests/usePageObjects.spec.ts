ae8ea337204e6aa60f43758f2f9a479b8e7fcc78
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-04-06 13:08:03 +0200

    date picker page object

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test';
 import { NavigationPage } from '../page-objects/navigationPage';
 import { FormLayoutsPage } from '../page-objects/formLayoutsPage';
+import { DatepickerPage } from '../page-objects/datepickerPage';
 
 
 
@@ -21,9 +22,15 @@ test('navigate to form page', async({page}) => {
 test('parametrized methods', async({page}) => {
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatePickerPage = new DatepickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', "Welcome1", "Option 2")
     await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', "John@test.com", false)
+    
+    await navigateTo.datepickerPage()
+    await onDatePickerPage.selectCommonDatePickerDateFromToday(5)
+    await onDatePickerPage.selectDatepickerWithRangeFromToday(6, 15)
 
 })
+
