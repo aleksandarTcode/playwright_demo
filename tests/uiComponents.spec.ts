5e5c75f9868bef6e07ee9891e2bd78264c17a9c9
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-05-21 11:08:36 +0200

    test retries

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -8,12 +8,18 @@ test.beforeEach(async ({page}) => {
 
 
 test.describe('Form Layouts page', () => {
+
+    // test.describe.configure({retries: 2})
+
     test.beforeEach(async ({page}) => {
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
     })
 
-    test('input fields', async({page}) => {
+    test('input fields', async({page}, testInfo) => {
+        // if(testInfo.retry){
+        //     //do something
+        // }
         const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
 
         await usingTheGridEmailInput.fill('test@test.com')
