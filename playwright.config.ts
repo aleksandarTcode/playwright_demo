86b0f8a5fecb7994075639fd27b0511b9fa670d7
aleksandarTcode <aleksandar.trmcic@gmail.com>
2026-05-23 09:42:25 +0200

    reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,6 +37,10 @@ export default defineConfig<TestOptions>({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  // [
+  //   ['json',{outputFile: 'test-results/jsonReport.json'}],
+  //   ['junit',{outputFile: 'test-results/jsonReport.xml'}],
+  // ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
